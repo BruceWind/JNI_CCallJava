a360be5c7a4d97be8459e8be4b7cb3a5a0213f74
weizongwei5 <weizongwei5@gmail.com>
2016-11-08 13:36:09 +0800

    c -> java

--- app/src/main/jni/com_androidyuan_tool_JavaCallC.c
+++ app/src/main/jni/com_androidyuan_tool_JavaCallC.c
@@ -18,5 +18,10 @@ JNIEXPORT jstring JNICALL Java_com_androidyuan_tool_JavaCallC_getString (JNIEnv 
 
 JNIEXPORT void JNICALL Java_com_androidyuan_tool_JavaCallC_setStreamListener (JNIEnv *env, jclass _jc, jobject _jobj)
 {
-	
+	   jclass ClassCJM = (*env)->FindClass(env, "com/androidyuan/ccalljava/MainActivity$OnStreamLiStener");
+	   //com.androidyuan.ccalljava.MainActivity$OnStreamLiStener
+	   //JVM 中内部类 被用 $ 符号分隔
+        jmethodID MethodDisplayMessage = (*env)->GetMethodID(env, ClassCJM, "onConnected", "()V");
+        (*env)->CallVoidMethod(env, _jobj, MethodDisplayMessage);
+
 }
